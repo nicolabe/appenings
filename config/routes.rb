97acd09263e6d2c36766780559124cde1bba1367
Nico <nico@ubuntu.(none)>
2013-03-25 02:08:42 +0100

    added default signin path

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Appenings::Application.routes.draw do
   devise_scope :user do
     get "/logout" => "devise/sessions#destroy"
   end
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
