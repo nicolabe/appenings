08d35039b1d5d39633c62e0fded4e57b262d09d8
Nico <nico@ubuntu.(none)>
2013-03-26 23:42:02 +0100

    ajaxified new and edit

--- app/controllers/appenings_controller.rb
+++ app/controllers/appenings_controller.rb
@@ -29,7 +29,7 @@ class AppeningsController < ApplicationController
 
     respond_to do |format|
       format.html # new.html.erb
-      format.json { render json: @appening }
+      format.js
     end
   end
 
