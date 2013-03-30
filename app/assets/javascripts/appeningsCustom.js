e372aac5c05c838debca570013d1f6f99f5881cd
Jeger <hal.egg@gmail.com>
2013-03-30 04:35:36 +0100

    styling of appening details, new animation on new appening

--- app/assets/javascripts/appeningsCustom.js
+++ app/assets/javascripts/appeningsCustom.js
@@ -5,10 +5,14 @@ $(document).ready(function() {
 	// $(this).append(" BAM! " + i);
 	// });
 
+$('div[id^="appening_"]').click(function() {
+	$(this).siblings('.appeningDetails').slideToggle('fast');
+});
+
 
 //toggle inn and out the field for adding an appening
 $('.newAppeningButton, #cancelAppening').click(function() {
-	$('#newAppening').slideToggle('fast', function() {
+	$('#newAppening').toggle('drop', function() {
 		$('.submit').on('click', function(){
 			$(this).closest("form").submit();
 		});
